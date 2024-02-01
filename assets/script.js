56c68e94ea8f1dfd6eb3869423d018b7e0049dd9
nil1143 <>
2024-02-01 10:49:29 +0000

    .js file added, files selected

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// query selector
+const choices = document.querySelectorAll('.choice');
+const userScoreSpan = document.getElementById('user-score');
+const computerScoreSpan = document.getElementById('computer-score');
+const resultMessage = document.getElementById('result');
